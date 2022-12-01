51c0628d460815e7ab412217797f22cb9d8bea4c
Erik Magnusson <erik.arn.magnusson@gmail.com>
2022-12-01 10:15:05 +0100

    clean

--- examples/day1.rs
+++ examples/day1.rs
@@ -2,29 +2,29 @@ fn main() {
     let s = include_str!("../input_data/day1.txt");
 
     // part 1
-    let a = s
-        .split("\n\n")
-        .map(|s| {
-            s.split("\n")
-                .map(|a| a.parse::<u32>().unwrap_or(0))
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
-    println!("max (part 1): {}", a);
+    {
+        s.split("\n\n")
+            .map(|s| {
+                s.split("\n")
+                    .filter_map(|a| a.parse::<u32>().ok())
+                    .sum::<u32>()
+            })
+            .max()
+            .map(|a| println!("max (part 1): {}", a));
+    }
 
     // part 2
-    let mut a = s
-        .split("\n\n")
-        .map(|s| {
-            s.split("\n")
-                .map(|a| a.parse::<u32>().unwrap_or(0))
-                .sum::<u32>()
-        })
-        .collect::<Vec<u32>>();
-    a.sort();
-    println!(
-        "top 3 sum (part 2): {}",
-        a[(a.len() - 3)..(a.len())].iter().sum::<u32>()
-    );
+    {
+        let mut a = s
+            .split("\n\n")
+            .map(|s| {
+                s.split("\n")
+                    .filter_map(|a| a.parse::<u32>().ok())
+                    .sum::<u32>()
+            })
+            .collect::<Vec<u32>>();
+        a.sort();
+        let a = a[(a.len() - 3)..(a.len())].iter().sum::<u32>();
+        println!("top 3 sum (part 2): {}", a);
+    }
 }
